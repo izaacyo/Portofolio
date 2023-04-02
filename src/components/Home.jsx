eb9aef43b8ef8ba1a34f530712177c6be666bb89
Marius Popa <marius.popa@druid.fi>
2023-04-02 18:55:37 +0200

    add margin on subtitle home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,10 +12,10 @@ const Home = () => {
       <div className='flex justify-center items-center w-full h-full'>
       <div className='max-w-[1000px] px-8 flex flex-col justify-center h-full'>
         <p className='text-pink-600'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+        <h1 className='text-4xl sm:text-7xl my-2 font-bold text-[#ccd6f6]'>
           Popa Marius
         </h1>
-        <h2 className='text-4xl sm:text-4xl font-bold text-[#8892b0]'>
+        <h2 className='text-4xl sm:text-4xl my-2 font-bold text-[#8892b0]'>
           I'm a Software Developer.
         </h2>
 
